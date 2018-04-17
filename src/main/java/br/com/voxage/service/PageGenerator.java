ecee1d90882dbef707859f1702c55eda6e266b00
Victor Hugo <victor.bello@voxage.com.br>
2018-04-17 19:04:15 -0300

    Desenvolvimento de layout para index e template do release notes
    @[Corrigido]
    Leitura da URL do gitblit. Estava utilizando o caminho errado.
    
    @[Novo]
    Implementação de menu fixo que aparecerá na tela ao utilzar a barra de rolagem
    Implementacao de funcionalidade para identificação de quebra com outros módulos
    
    @[Alterado]
    Alterado para utilizar a versão obtida a partir da TAG em vez da versão definidia no POM.xml.
    
    @[Informações técnicas]
    Melhorias gerais no codigo
    
    @[Quebra(n)]
    
    @[Demandas]
    N/A
    
    @[Tarefa]
    63521

--- src/main/java/br/com/voxage/service/PageGenerator.java
+++ src/main/java/br/com/voxage/service/PageGenerator.java
@@ -56,6 +56,8 @@ public class PageGenerator implements Generator {
     public PageGenerator(String fromPath, String branch) {
         this.filePath = new File(fromPath);
         this.releaseType = ReleaseType.fromBranch(branch);
+        System.out.println("Caminho: " + fromPath + " | branch: " + branch);
+        System.out.println("ReleaseType: " + releaseType);
     }
 
     public PageGenerator(String fromPath) {
@@ -68,21 +70,22 @@ public class PageGenerator implements Generator {
         String[] list = filePath.list(filter);
         if (list != null && list.length > 0) {
 
-            StringBuilder sb = new StringBuilder();
+            StringBuilder span = new StringBuilder();
+            StringBuilder option = new StringBuilder();
             StringBuilder iframe = new StringBuilder();
             for (String fileName : list) {
                 Matcher matcher = releaseType.getPatternVersion().matcher(fileName);
                 if (matcher.find()) {
                     String version = matcher.group();
-                    sb.append("<span>").append("<a href='").append(fileName).append("'>").append(version).append("</a>").append("</span>");
-
+                    span.append("<div class='col-lg-1 col-sm-2 col-xs-3'>").append("<a href='").append(fileName).append("'>").append(version).append("</a>").append("</div>");
+                    option.append("<option value='").append(fileName).append("'>").append(version).append("</option>");
                     iframe.append("<div>").append("<iframe id='").append(version).append("' src='").append(fileName).append("'>").append("</iframe>").append("</div>");
                 }
             }
 
             String html = ReadResource.read("index.html");
 
-            String format = html.replace("$(indice)", sb.toString()).replace("$(iframes)", iframe.toString());
+            String format = html.replace("$(indice)", span.toString()).replace("$(fixed-indice)", option.toString()).replace("$(iframes)", iframe.toString());
             Files.write(Paths.get(filePath + File.separator + "index.html"), format.getBytes());
 
         }
